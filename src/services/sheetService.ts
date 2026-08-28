5680fcf54cecd13ad1a120d4557365b5210c0b68
WLDN <wldn@email.com>
2026-08-28 08:39:35 +0700

    chore: update Google Apps Script Web App URL in sheetService.ts

--- src/services/sheetService.ts
+++ src/services/sheetService.ts
@@ -3,7 +3,7 @@
  * Menghubungkan aplikasi ke Google Spreadsheet via Apps Script (OTP & Data Sync)
  */
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxqOmVTaHSoCSrhzzk3WUHH4TyIFKS8Ok62D-mTxbxlS8rb5-9f5gT-IOBQMmBgNANq/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx3eOAnSUGMBl8uws6VyT5SgW2NXNVXEuS_5JjCqtryWHMJie7imulRs3lmUNpzoHMi/exec';
 
 export interface RegistrationData {
   type: 'registration';
